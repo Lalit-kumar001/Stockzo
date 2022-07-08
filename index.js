f79277136e91bb2a9c444d4557f28c63f0aa4db1
Lalit-kumar001 <96826866+Lalit-kumar001@users.noreply.github.com>
2022-07-08 22:19:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+
+var on=0;
+var home=document.getElementById('home');
+home.addEventListener('click',function(){
+    home.classList.add('border');
+})
+
+
+var menu=document.querySelector('.menu');
+var ham=document.getElementById('hamburger');
+ham.addEventListener('click',function(){
+if(on==0){
+
+    menu.style.display="block";
+    on=1;
+
+}
+else{
+    menu.style.display="none";
+    on=0;
+}
+
+})
